5b22d88db057b63a1d1ea0356c2d6ebfac8cef27
James Lovallo <lovallo.james@gmail.com>
2022-09-09 17:16:37 -0400

    change codemirror theme

--- docs/assets/playground.js
+++ docs/assets/playground.js
@@ -26,7 +26,7 @@ const coreConfig = {
 	lineNumbers: true,
 	scrollbarStyle: 'native',
 	tabSize: 2,
-	theme: 'dracula',
+	theme: 'shadowfox',
 }
 
 fetch(`/ardi/components/${component || 'helloWorld'}.js`)
