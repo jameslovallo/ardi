0e88f6650363f32dc160b93079477e1beaada03e
James Lovallo <lovallo.james@gmail.com>
2022-09-12 01:30:02 -0400

    .

--- ardi.js
+++ ardi.js
@@ -1,64 +1,74 @@
 export default (c) => {
+	const props = Object.keys(c.props)
 	class ardi extends HTMLElement {
-		connectedCallback() {
-			Object.assign(this, c)
-			this.render = () => {
-				this.DOM = this.shadow ? this.attachShadow({ mode: 'open' }) : this
+		constructor() {
+			super() && Object.assign(this, c)
+			this.DOM = c.shadow ? this.attachShadow({ mode: 'open' }) : this
+		}
 
-				if (this.props) {
-					Object.keys(this.props()).forEach((prop) => {
-						const handler = this.props()[prop]
-						const value = this.getAttribute(prop)
-						if (typeof handler === 'function') {
-							this[prop] = handler(value)
-						} else this[prop] = value ? value : handler
-					})
-				}
+		render() {
+			props.forEach((prop) => {
+				const handler = this.props()[prop]
+				const value = this.getAttribute(prop)
+				if (typeof handler === 'function') {
+					this[prop] = handler(value)
+				} else this[prop] = value ? value : handler
+			})
 
-				const css = this.styles ? `<style>${this.styles()}</style>` : ''
-				let html = this.template ? this.template() : ''
-				if (html?.nodeType === 1) html = html.outerHTML
-				this.DOM.innerHTML = css + html
-				html.match(/@[a-z]+/gi)?.forEach((eAttr) => {
-					const eString = eAttr.replace('@', '')
-					this.DOM.querySelectorAll(`[\\${eAttr}]`).forEach((el) => {
-						let func = this[el.getAttribute(eAttr)]
-						if (typeof this[el.getAttribute(eAttr)] === 'function') {
-							el.removeAttribute(eAttr)
-							el.addEventListener(eString, (e) => {
-								func.apply(this, [e])
-							})
-						}
-					})
-				})
+			const css = this.styles ? `<style>${this.styles()}</style>` : ''
+			let html = this.template ? this.template() : ''
+			if (html?.nodeType === 1) html = html.outerHTML
+			this.DOM.innerHTML = css + html
 
-				this.refs = {}
-				this.DOM.querySelectorAll('[ref]').forEach((ref) => {
-					ref.on = (type, func = func) => {
-						ref.addEventListener(type, (e) => {
+			html.match(/@[a-z]+/gi)?.forEach((eAttr) => {
+				const eString = eAttr.replace('@', '')
+				this.DOM.querySelectorAll(`[\\${eAttr}]`).forEach((el) => {
+					let func = this[el.getAttribute(eAttr)]
+					if (typeof this[el.getAttribute(eAttr)] === 'function') {
+						el.removeAttribute(eAttr)
+						el.addEventListener(eString, (e) => {
 							func.apply(this, [e])
 						})
 					}
-					this.refs[ref.getAttribute('ref')] = ref
 				})
+			})
 
-				this.ready && this.ready()
-
-				if (typeof this.intersect === 'function') {
-					new IntersectionObserver(
-						(entries) =>
-							entries.forEach((entry) => {
-								entry.isIntersecting && this.intersect(entry.intersectionRatio.toFixed(2))
-							}),
-						{
-							root: null,
-							rootMargin: '0px',
-							threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-						}
-					).observe(this)
+			this.refs = {}
+			this.DOM.querySelectorAll('[ref]').forEach((ref) => {
+				ref.on = (type, func) => {
+					ref.addEventListener(type, (e) => {
+						func.apply(this, [e])
+					})
 				}
-			}
+				this.refs[ref.getAttribute('ref')] = ref
+			})
 
+			this.ready && this.ready()
+
+			if (typeof this.intersect === 'function') {
+				new IntersectionObserver(
+					(entries) =>
+						entries.forEach((entry) => {
+							entry.isIntersecting && this.intersect(entry.intersectionRatio.toFixed(2))
+						}),
+					{
+						root: null,
+						rootMargin: '0px',
+						threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+					}
+				).observe(this)
+			}
+		}
+
+		connectedCallback() {
+			this.render()
+		}
+
+		static get observedAttributes() {
+			if (c.reactive) return Object.keys(c.props())
+		}
+
+		attributeChangedCallback() {
 			this.render()
 		}
 	}
