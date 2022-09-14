3a91e1c031321afcef3ba14edc2cf507411755b0
James Lovallo <lovallo.james@gmail.com>
2022-09-14 00:04:16 -0400

    default values fix

--- docs/js/components/contactCard.js
+++ docs/js/components/contactCard.js
@@ -95,7 +95,7 @@ export default class contactCard extends ardi {
 			text-decoration: none;
 		}
 		[part=contact] svg {
-			color: #D32F2F;
+			color: #00B8D4;
 			display: block;
 			width: 20px;
 			pointer-events: none;
