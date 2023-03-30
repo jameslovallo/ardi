31985aa923afc7effd5702a2ef16dfb8c6a65380
James Lovallo <patrickonparker@gmail.com>
2023-03-30 10:07:51 -0400

    refactor(md.js): extract split string into a constant variable

--- @/components/md.js
+++ @/components/md.js
@@ -25,7 +25,8 @@ ardi({
         const nameArray = this.src.split('.')
         const lang = nameArray[nameArray.length - 1]
         let md = lang === 'md' ? text : codeToMd(lang, text)
-        if (md.includes('<!-- split -->')) md = md.split('<!-- split -->')[1]
+        const split = `<!-- ${'split'} -->`
+        if (md.includes(split)) md = md.split(split)[1]
         const hasCodeBlocks = md.includes('```')
         this.root.innerHTML = `
           ${hasCodeBlocks ? `<style>@import "${this.theme}";</style>` : ''}
