ee716b3fceccc5c972f2ef8b7b8e3f1de8da10e4
James Lovallo <patrickonparker@gmail.com>
2023-03-30 10:47:12 -0400

    style(grid.js): adjust grid margins to be consistent across screen sizes

--- @/components/grid.js
+++ @/components/grid.js
@@ -9,12 +9,13 @@ ardi({
 		:host {
 			display: grid;
 			gap: 1rem;
-			margin-block: 1rem;
+			margin: 1rem 0;
 			text-align: center;
 		}
 		@media (min-width: 600px) {
 			:host {
 				grid-template-columns: repeat(3, 1fr);
+				margin: 1rem -1rem;
 			}
 		}
 	`,
