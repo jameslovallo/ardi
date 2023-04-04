8bb25d17df8d9b6b4633410c0b72c93e36eeb733
James Lovallo <patrickonparker@gmail.com>
2023-04-04 13:24:19 -0400

    add bible demo

---
+++ demos/verses/verses.js
@@ -0,0 +1,29 @@
+import ardi from '/@/assets/ardi-min.js'
+
+ardi({
+  tag: 'ardi-verses',
+  shadow: false,
+  props: { query: [String, 'John 1 1-34'] },
+  state: () => ({ html: '' }),
+  propChange() {
+    fetch(
+      ['https://api.esv.org', '/v3/passage/html', '/?q=', this.query].join(''),
+      {
+        headers: {
+          Authorization: 'Token 9440aa524a886936a723a0496edaf2d406df48a9',
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => (this.html = json.passages[0]))
+  },
+  template() {
+    return this.html
+  },
+  css: `
+		@import url('https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap');
+		h2, h3 {
+			font-family: Cinzel Decorative
+		}
+	`,
+})
