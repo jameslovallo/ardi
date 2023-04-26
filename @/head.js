4a735268d2e653316bbf47da93d418d13f2766bb
James Lovallo <patrickonparker@gmail.com>
2023-04-26 12:50:47 -0400

    fix verses component

--- @/head.js
+++ @/head.js
@@ -20,10 +20,5 @@ export default {
       rel: 'icon',
       href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐵</text></svg>',
     },
-    {
-      rel: 'preload',
-      as: 'image',
-      href: '/@/assets/fast.svg',
-    },
   ],
 }
--- demos/verses/verses.js
+++ demos/verses/verses.js
@@ -4,17 +4,20 @@ ardi({
   tag: 'ardi-verses',
   props: { query: [String, 'John 1 1-34'] },
   state: () => ({ html: '' }),
-  changed() {
-    fetch(
-      ['https://api.esv.org', '/v3/passage/html', '/?q=', this.query].join(''),
-      {
-        headers: {
-          Authorization: 'Token 9440aa524a886936a723a0496edaf2d406df48a9',
-        },
-      }
-    )
+  fetchVerses() {
+    fetch(`https://api.esv.org/v3/passage/html/?q=${this.query}`, {
+      headers: {
+        Authorization: 'Token 9440aa524a886936a723a0496edaf2d406df48a9',
+      },
+    })
       .then((res) => res.json())
       .then((json) => (this.html = json.passages[0]))
+  },
+  created() {
+    this.fetchVerses()
+  },
+  changed() {
+    this.fetchVerses()
   },
   template() {
     return this.html
