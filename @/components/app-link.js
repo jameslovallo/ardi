f816efb7ccba801b6cddd40f57309b3e95f66b7f
James Lovallo <patrickonparker@gmail.com>
2023-04-27 14:49:40 -0400

    test app-link fix

--- @/components/app-link.js
+++ @/components/app-link.js
@@ -10,13 +10,18 @@ ardi({
     this.addEventListener('mouseover', (e) => this.hover(e))
     this.addEventListener('click', (e) => this.click(e))
   },
-  fetchPage(href) {
+  fetchPage(href, setPage) {
     fetch((href !== '/' ? href : '') + '/index.html')
       .then((res) => res.text())
       .then((page) => {
         this.doc = page.split('<app-layout>')[1].split('</app-layout>')[0]
         sessionStorage.setItem(href, this.doc)
+        setPage && this.setPage()
       })
+  },
+  setPage() {
+    appRoot.setPage(this.href, this.doc)
+    appRoot.pushHistory(this.href)
   },
   hover(e) {
     e.preventDefault()
@@ -30,10 +35,6 @@ ardi({
   click(e) {
     e.preventDefault()
     const href = this.href.split('#')[0]
-    if (this.href !== location.pathname) {
-      if (!this.doc) this.fetchPage(href)
-      appRoot.setPage(this.href, this.doc)
-      appRoot.pushHistory(this.href)
-    }
+    if (!this.doc) this.fetchPage(href, true)
   },
 })
