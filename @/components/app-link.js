082705bde2ad1af304089a6d910790b4b168f410
James Lovallo <patrickonparker@gmail.com>
2023-04-27 14:45:00 -0400

    test app-link fix

--- @/components/app-link.js
+++ @/components/app-link.js
@@ -10,23 +10,28 @@ ardi({
     this.addEventListener('mouseover', (e) => this.hover(e))
     this.addEventListener('click', (e) => this.click(e))
   },
+  fetchPage(href) {
+    fetch((href !== '/' ? href : '') + '/index.html')
+      .then((res) => res.text())
+      .then((page) => {
+        this.doc = page.split('<app-layout>')[1].split('</app-layout>')[0]
+        sessionStorage.setItem(href, this.doc)
+      })
+  },
   hover(e) {
     e.preventDefault()
     const href = this.href.split('#')[0]
     if (Object.keys(sessionStorage).includes(href)) {
       this.doc = sessionStorage[this.href]
     } else {
-      fetch((href !== '/' ? href : '') + '/index.html')
-        .then((res) => res.text())
-        .then((page) => {
-          this.doc = page.split('<app-layout>')[1].split('</app-layout>')[0]
-          sessionStorage.setItem(this.href, this.doc)
-        })
+      this.fetchPage(href)
     }
   },
   click(e) {
     e.preventDefault()
+    const href = this.href.split('#')[0]
     if (this.href !== location.pathname) {
+      if (!this.doc) this.fetchPage(href)
       appRoot.setPage(this.href, this.doc)
       appRoot.pushHistory(this.href)
     }
