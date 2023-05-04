c723a332202e0a8c5073f62fc1a135ac99a18717
James Lovallo <patrickonparker@gmail.com>
2023-05-03 23:46:13 -0400

    Update fullpage.js

--- demos/fullpage/fullpage.js
+++ demos/fullpage/fullpage.js
@@ -5,7 +5,7 @@ ardi({
 
   props: {
     activeindicator: [String],
-    activeindicatorcss: [String, 'transform: scale(1.75)'],
+    activeindicatorstyle: [String, 'transform: scale(1.75)'],
     alignment: [String, 'center'], // left center right
     indicator: [String, '●'],
     indicatorcolor: [String, 'currentcolor'],
